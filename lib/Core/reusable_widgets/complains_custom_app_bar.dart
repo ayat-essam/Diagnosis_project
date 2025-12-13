30e03c07f361c465fef0414c82391eb2943d4f92
Doaa-Atef <doaa.atef.hemed@gmail.com>
2025-12-13 07:16:51 +0200

    Complains_Screen

--- lib/Core/reusable_widgets/complains_custom_app_bar.dart
+++ lib/Core/reusable_widgets/complains_custom_app_bar.dart
@@ -84,6 +84,7 @@ class ComplaintsCustomAppBar extends StatelessWidget {
                 ,
 
 
+
                 SizedBox(width: 14.w),
                 const CircleAvatar(
                   backgroundColor: AppColors.BluePrimary,

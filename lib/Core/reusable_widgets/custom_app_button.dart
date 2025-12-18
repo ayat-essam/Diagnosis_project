fee0e80a85f0522aa9e4a7d8645d6c9fc15926f2
Aya Nasser <ayanasser1853@gmail.com>
2025-12-19 01:58:37 +0200

    edit diagnosis module screen

--- lib/Core/reusable_widgets/custom_app_button.dart
+++ lib/Core/reusable_widgets/custom_app_button.dart
@@ -12,26 +12,32 @@ class CustomAppButton extends StatelessWidget {
     required this.text,
     this.borderRedius = 14,
     this.width = double.infinity,
-    this.backgroundColor=AppColors.BluePrimary,
+    this.backgroundColor = AppColors.BluePrimary,
   });
-final String text;
-final double borderRedius;
-final double width;
-final Color backgroundColor;
+  final String text;
+  final double borderRedius;
+  final double width;
+  final Color backgroundColor;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
         width: width,
-        padding: EdgeInsets.symmetric(vertical: 3.h),
+        padding: EdgeInsets.symmetric(vertical: 8.h),
         decoration: BoxDecoration(
           color: backgroundColor,
           borderRadius: BorderRadius.circular(borderRedius),
-          border: Border.all(color: AppColors.BluePrimary, ),
+          border: Border.all(
+            color: AppColors.BluePrimary,
+          ),
         ),
         child: Center(
-          child: Text(text, style:backgroundColor==AppColors.BluePrimary?StyleApp.font12WhiteSemiBold: StyleApp.font13BlueSemiBold),
+          child: Text(text,
+              style: backgroundColor == AppColors.BluePrimary
+                  ? StyleApp.font14BlueSemiBold
+                      .copyWith(color: AppColors.whiteBackground)
+                  : StyleApp.font14BlueSemiBold),
         ),
       ),
     );
